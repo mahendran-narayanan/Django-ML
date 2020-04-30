17fbd0014407d0be0f584c48057640428c47d27e
mahendran-narayanan <mahendranNNM@gmail.com>
2020-04-30 13:25:13 +0530

    Created form

--- ml/reg/models.py
+++ ml/reg/models.py
@@ -1,5 +1,27 @@
 from django.db import models
-
+from sklearn import linear_model
+import numpy as np
 # Create your models here.
 class Data(models.Model):
-	dataset = models.CharField(max_length=10000)
+	x = models.CharField(max_length=10000)
+	y = models.CharField(max_length=10000,default="0")
+	testx = models.CharField(max_length=10000,default="0")
+	def ab(self):
+		train_x = self.x
+		train_x = train_x.split(",")
+		train_x = list(map(float,train_x))
+		train_x = np.asarray(train_x)
+		train_x = train_x.reshape(-1,1)
+		
+		train_y = self.y
+		train_y = train_y.split(",")
+		train_y = list(map(float,train_y))
+		train_y = np.asarray(train_y)
+		train_y = train_y.reshape(-1,1)
+		
+		model = linear_model.LinearRegression()
+		model.fit(train_x,train_y)
+		test_x = self.testx
+		test_x = test_x.reshape(-1,1)
+		res = model.predict(test_x)
+		return res
--- ml/reg/views.py
+++ ml/reg/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import DataForm
+from .models import Data
 # Create your views here.
 def index(request):
 	if request.method == 'POST':
 		form = DataForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return HttpResponse('Data Added to DB')
+			inst = Data.objects.reverse()[0]
+			res = Data.ab(inst)
+			return render(request,'processed.html',{'rdata':form})
 	else:
 		form = DataForm()
 	return render(request,'add_data.html',{'datap': form})
